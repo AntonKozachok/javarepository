7d56bce774bdd7214dc77be3362c71dce8b0e409
AntonKozachok <Anton.Kozachok@gmail.com>
2020-02-17 14:13:20 +0200

    lesson4 commit

---
+++ src/lesson04/a_inheritance/Program.java
@@ -0,0 +1,13 @@
+package lesson04.a_inheritance;
+
+public class Program {
+    public static void main(String[] args) {
+        Truck truck = new Truck( "Renault",2005);
+        PassengerCar car = new PassengerCar("Daewoo", 2016);
+        System.out.println(truck);System.out.println(car);
+        truck.gas();
+        car.gas();
+        truck.stop();
+        car.stop();
+    }
+}
