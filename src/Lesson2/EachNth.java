658e91fae801bcec2cc1f8b07f31c6f30330e47d
anton kozachok <akozachok@deloitte.com>
2020-03-07 22:25:54 +0200

    Assignment with each Nth element of String

---
+++ src/Lesson2/EachNth.java
@@ -0,0 +1,19 @@
+package Lesson2;
+
+import org.jetbrains.annotations.NotNull;
+
+public class EachNth {
+    public String eachNth(int n, String str) {
+        char[] medium = str.toCharArray();
+        String result="";
+        for (int i = 0; i<=str.length(); i += n) result = result.concat(String.valueOf(medium[i]));
+        return result;
+    }
+
+    public static void main(String[] args) {
+        EachNth otpt = new EachNth();
+        otpt.eachNth(2,"voyodonooo");
+        System.out.println("String with each Nth element is");
+        System.out.printf("%s",otpt);
+    }
+}
