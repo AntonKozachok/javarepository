d1e7e18349798d6d349c78d5eb4512c1e253df04
anton kozachok <akozachok@deloitte.com>
2020-03-10 11:15:23 +0200

    Fix Fibonacci

--- src/Lesson3/Fibonacci.java
+++ src/Lesson3/Fibonacci.java
@@ -1,20 +1,34 @@
 package Lesson3;
 
 public class Fibonacci {
-    public Fibonacci() {    }
+    public Fibonacci() {
+    }
 
     public static void printFibonacciNumbers(int howMany) {
-        int[] fibonacciNumbers = new int[howMany];
-        if (howMany <= 0) System.out.println("Enter positive number");
+        if (howMany <= 0) {
+            System.out.println("Enter positive number");
+            return;
+        }
+        long[] fibonacciNumbers = new long[howMany];
+
+        if (howMany == 1) {
+            System.out.println("Fibonacci numbers:"+0);
+            return;
+        }
+
         fibonacciNumbers[0] = 0;
         fibonacciNumbers[1] = 1;
+        System.out.println("Fibonacci numbers:");
         for (int i = 2; i <= howMany - 1; i++)
             fibonacciNumbers[i] = fibonacciNumbers[i - 1] + fibonacciNumbers[i - 2];
-        for (int j = 0; j <= howMany; j++) System.out.println(fibonacciNumbers[j] + " ");
+        for (int j = 0; j < howMany; j++) System.out.println(fibonacciNumbers[j] + " ");
     }
 
     public static void main(String[] args) {
-        printFibonacciNumbers(4);
+        printFibonacciNumbers(78);
     }
 }
+
+
+
 
